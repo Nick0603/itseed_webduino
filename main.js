20699a832fd45adb9f5c0de810f912a24d5c22ba
Nick0603 <b10303008@gmail.com>
2018-07-27 23:43:02 +0800

    inititally created

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var servo;
+
+
+boardReady({board: 'Smart', device: '10yswpNaV', transport: 'mqtt'}, async function (board) {
+  board.systemReset();
+  board.samplingInterval = 50;
+  servo = getServo(board, 14);
+  document.getElementById('connected-label').innerHTML = '連線成功！';
+  servo.angle = 180;
+  document.getElementById("action-btn").addEventListener("click",async function(){
+    servo.angle = 0;
+    await delay(1);
+    servo.angle = 180;
+  });
+  
+  board.on('error', function (err) {
+    board.error = err;
+    document.getElementById('connected-label').innerHTML = '斷線！ 請重新整理頁面';
+  });
+});
